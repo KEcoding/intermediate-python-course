0333783f13ef7ab856caa929e68aec032de15831
Kyana Burhite <kyanaburhite@Kyanas-MacBook-Pro.local>
2020-08-06 11:49:06 -0500

    input statements

--- dice_roller.py
+++ dice_roller.py
@@ -1,11 +1,21 @@
 import random
 def main():
-  dice_rolls = 2 
+  dice_rolls = int(input("How many dice would you like to roll?")) 
+  dice_size = int(input("how many sides do the dice have?"))
+
   dice_sum = 0 
   for i in range (0, dice_rolls):
-    roll = random.randint(1,6)
+    roll = random.randint(1,dice_size)
     dice_sum = dice_sum + roll
-    print(f'You rolled a {roll} !')
+    if roll == 1:
+      print("Ew, you have a one! Game over!")
+      break
+    elif roll == dice_size:
+      print("you have a 6! Critical success!")
+
+    else:
+      print(f'You rolled a {roll} !')
+      
     
   print(f'You have a sum of {dice_sum}')
 
